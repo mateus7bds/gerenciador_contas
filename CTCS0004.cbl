277da8128a069bd236461509764a8a0035d30cfd
Mateus Barbosa da Silva <mateusbarbosadasilva7@gmail.com>
2026-06-09 11:53:07 -0300

    fluxo completo da trns finalizado - falta simular o rollback

--- CTCS0004.cbl
+++ CTCS0004.cbl
@@ -28,12 +28,20 @@
            FILE STATUS  IS W-CNT001-FILE-STATUS
            .
       *
+       SELECT TRNS001
+           ASSIGN TO 'C:\Users\F7021226\Documents\transferencias.dat'
+           ORGANIZATION IS INDEXED
+           ACCESS MODE  IS RANDOM
+           RECORD KEY   IS TRNS001-ID
+           FILE STATUS  IS W-TRNS001-FILE-STATUS
+           .
+      *
       *------------------------------------------------------------------------
        DATA DIVISION.
       *------------------------------------------------------------------------
        FILE SECTION.
       *------------------------------------------------------------------------
-       FD CNT001.
+       FD  CNT001.
       *
        01  CNT001-REGISTRO.
            03  CNT001-ID-CLI.
@@ -46,11 +54,23 @@
            03  CNT001-DT-NSC                  PIC  X(008).
            03  CNT001-SDO                     PIC  9(015)V99.
       *
+       FD  TRNS001.
+      *
+       01  TRNS001-REGISTRO.
+           03  TRNS001-ID                     PIC  9(018).
+           03  TRNS001-AG-RMTE                PIC  9(004).
+           03  TRNS001-CT-RMTE                PIC  9(008).
+           03  TRNS001-AG-BNFC                PIC  9(004).
+           03  TRNS001-CT-BNFC                PIC  9(008).
+           03  TRNS001-VL-TRNS                PIC  9(015)V99.
+           03  TRNS001-TS-TRNS                PIC  X(016).
+      *
       *------------------------------------------------------------------------
        WORKING-STORAGE SECTION.
       *------------------------------------------------------------------------
       *
        77  W-CNT001-FILE-STATUS               PIC  X(002) VALUE ZEROS.
+       77  W-TRNS001-FILE-STATUS              PIC  X(002) VALUE ZEROS.
       *
       *------------------------------------------------------------------------
        LOCAL-STORAGE SECTION.
@@ -71,6 +91,13 @@
            03  L-CT-CLI-VLDC                   PIC  9(008) VALUE ZEROS.
            03  L-TIPO-CLI                      PIC  X(020) VALUE SPACES.
       *
+       01  L-FIM-TRNS001                       PIC  X(001).
+           88  L-FIM-SIM-TRNS                  VALUE "S".
+           88  L-FIM-NAO-TRNS                  VALUE "N".
+      *
+       77  L-ID-ULT-REG-TRANS001               PIC  9(018) VALUE ZEROS.
+       77  L-ID-NOVO-REG-TRNS001               PIC  9(018) VALUE ZEROS.
+      *
       *------------------------------------------------------------------------
       * LINKAGE SECTION.
       *------------------------------------------------------------------------
@@ -84,7 +111,6 @@
                05  CTCS0004-CT-BNFC          PIC  9(008) VALUE 87654321.
                05  CTCS0004-VL-TRNS          PIC  9(015)V99
                    VALUE 200,40.
-      *
            03  CTCS0004-VRV-RTN.
                05  CTCS0004-CD-RTN             PIC  9(004) VALUE ZEROS.
                05  CTCS0004-TX-MSG-RTN         PIC  X(080) VALUE SPACES.
@@ -102,17 +128,13 @@
            PERFORM 030000-VALIDAR-DD-ENTD
            PERFORM 040000-DEBITAR-REMETENTE
            PERFORM 050000-CREDITAR-BENEFICIARIO
-      *
-      * DEBITAR DO ORDENANTE
-      *    PERFORM 030000-DEBITAR-REMETENTE
-      * CREDITAR NA BENEFICIARIO
-      *    PERFORM 040000-CREDITAR-BENEFICIARIO
-      * REGISTRAR TRANSFERENCIA
-      *    PERFORM 050000-REGISTRAR-TRANSFERENCIA
+           PERFORM 060000-REGISTRAR-TRANSFERENCIA
            .
       *
        000000-FINALIZAR.
-           CLOSE CNT001
+           CLOSE CNT001 TRNS001
+           DISPLAY "CTCS0004-CD-RTN....: " CTCS0004-CD-RTN
+           DISPLAY "CTCS0004-TX-MSG-RTN: " CTCS0004-TX-MSG-RTN
            GOBACK
            .
       *------------------------------------------------------------------------
@@ -139,6 +161,17 @@
                OPEN OUTPUT CNT001
                CLOSE CNT001
                OPEN I-O CNT001
+           END-IF
+      *
+           OPEN I-O TRNS001
+      *
+      * FILE-STATUS - "35" - Arquivo inexistente
+      *
+           IF W-TRNS001-FILE-STATUS EQUAL "35"
+               CLOSE TRNS001
+               OPEN OUTPUT TRNS001
+               CLOSE TRNS001
+               OPEN I-O TRNS001
            END-IF
       *
            .
@@ -277,10 +310,14 @@
       *
            ADD
               CTCS0004-VL-TRNS TO CNT001-SDO
+              ON SIZE ERROR
+                  DISPLAY 'ERRO - CREDITAR VALOR NO BNFC'
+                  PERFORM 000000-FINALIZAR
            END-ADD
       *
            REWRITE CNT001-REGISTRO
-               NOT INVALID KEY
+               INVALID KEY
+                   DISPLAY 'FILE-STATUS: ' W-CNT001-FILE-STATUS
                    DISPLAY 'ERRO ATUALIZAR - BASE DADOS - BENEFICIARIO'
                    PERFORM 000000-FINALIZAR
            END-REWRITE
@@ -288,5 +325,59 @@
            .
       *
        050000-SAIR.
+           EXIT SECTION
+           .
+      *
+      *------------------------------------------------------------------------
+       060000-REGISTRAR-TRANSFERENCIA SECTION.
+      *------------------------------------------------------------------------
+      *
+           INITIALIZE TRNS001-REGISTRO
+      *
+           MOVE ZEROS TO L-ID-ULT-REG-TRANS001
+      *
+           PERFORM UNTIL L-FIM-TRNS001 EQUAL "S"
+               READ TRNS001 NEXT
+                   AT END
+                       MOVE TRNS001-ID TO L-ID-ULT-REG-TRANS001
+                       SET L-FIM-SIM-TRNS TO TRUE
+               END-READ
+           END-PERFORM
+      *
+           DISPLAY 'L-ID-ULT-REG-TRANS001: ' L-ID-ULT-REG-TRANS001
+      *
+           IF L-ID-ULT-REG-TRANS001 EQUAL ZEROS
+               MOVE 1 TO L-ID-NOVO-REG-TRNS001
+           ELSE
+               ADD
+                   L-ID-ULT-REG-TRANS001
+                   1
+                   GIVING L-ID-NOVO-REG-TRNS001
+                   ON SIZE ERROR
+                       DISPLAY 'NUMERO DE REG. DE TRNS ALCANCOU O LIMIT'
+               END-ADD
+           END-IF
+      *
+           MOVE L-ID-NOVO-REG-TRNS001 TO TRNS001-ID
+           MOVE CTCS0004-AG-RMTE      TO TRNS001-AG-RMTE
+           MOVE CTCS0004-CT-RMTE      TO TRNS001-CT-RMTE
+           MOVE CTCS0004-AG-BNFC      TO TRNS001-AG-RMTE
+           MOVE CTCS0004-CT-BNFC      TO TRNS001-CT-BNFC
+           MOVE CTCS0004-VL-TRNS      TO TRNS001-VL-TRNS
+           MOVE L-TS-CRR              TO TRNS001-TS-TRNS
+      *
+           WRITE TRNS001-REGISTRO
+               INVALID KEY
+                   DISPLAY 'FILE-STATUS-TRNS001: ' W-TRNS001-FILE-STATUS
+                   DISPLAY 'ERRO AO SALVAR REGISTRO - TRNS001'
+                   PERFORM 000000-FINALIZAR
+           END-WRITE
+      *
+           MOVE ZEROS TO CTCS0004-CD-RTN
+           MOVE "CTCS0004 - Transfencia executada com sucesso." TO
+               CTCS0004-TX-MSG-RTN
+           .
+      *
+       060000-SAIR.
            EXIT SECTION
            .
